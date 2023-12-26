75fd5a261da0d86973a5e35f8c2a79d7baaffe24
utkarsh032 <utkarshraj525@gmail.com>
2023-12-26 16:20:07 +0530

    Header(NavLink)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,19 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
 import Users from './user/pages/Users'
 import NewPlaces from './places/pages/NewPlaces'
+import MainNavigation from './shared/components/Navigation/MainNavigation/MainNavigation';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path='/' exact element={<Users />} />
-        <Route path='/places/new' exact element={<NewPlaces />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+      <MainNavigation />
+      <main>
+        <Routes>
+          <Route path='/' exact element={<Users />} />
+          <Route path='/places/new' exact element={<NewPlaces />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </main>
     </Router>
   );
 }
--- frontend/src/user/components/userslist/UsersList.js
+++ frontend/src/user/components/userslist/UsersList.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import UsersItem from '../usersitem/UsersItem'
 import './UsersList.css'
+import Card from '../../../shared/components/UIElements/Card/Card';
 
 const UsersList = props => {
   if (props.items.length === 0) {
     return (
       <div className="center">
-        <h2>No-user found</h2>
+        <Card>
+          <h2>No-user found</h2>
+        </Card>
       </div>
     );
   }
