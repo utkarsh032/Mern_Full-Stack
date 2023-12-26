4fa9c37cb038f0267828be644522255b4ef9ad3a
utkarsh032 <utkarshraj525@gmail.com>
2023-12-26 21:01:29 +0530

    Handling Drawe

--- frontend/src/shared/components/Navigation/MainNavigation/MainNavigation.js
+++ frontend/src/shared/components/Navigation/MainNavigation/MainNavigation.js
@@ -1,19 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Link } from 'react-router-dom'
 import MainHeader from '../MainHeader/MainHeader'
 import "./MainNavigation.css"
 import NavLinks from '../NavLinks/NavLinks'
 import SideDrawer from '../SideDrawer/SideDrawer'
+import Backdrop from '../../UIElements/Backdrop/Backdrop'
+
 
 const MainNavigation = props => {
+
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false)
+
+  const openDrawer = () => {
+    setDrawerIsOpen(true)
+  }
+
+  const closeDrawer = () => {
+    setDrawerIsOpen(false)
+  }
+
   return (
     <>
-      <SideDrawer>
-        <nav className='main-navigation__drawer-nav'><NavLinks /></nav>
-      </SideDrawer>
+      {drawerIsOpen && <Backdrop onClick={closeDrawer} />}
+      {drawerIsOpen && (
+        <SideDrawer>
+          <nav className='main-navigation__drawer-nav'><NavLinks /></nav>
+        </SideDrawer>)}
       <MainHeader>
-        <button className='main-navigation__menu-btn'>
+        <button className='main-navigation__menu-btn' onClick={openDrawer}>
           <span />
           <span />
           <span />
