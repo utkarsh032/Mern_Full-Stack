0b6666b75a3ac7011cc99f6c760410f7b31c67b4
utkarsh032 <utkarshraj525@gmail.com>
2024-01-01 21:35:43 +0530

    Adding_Input_Validation

--- frontend/src/places/pages/NewPlaces/NewPlaces.js
+++ frontend/src/places/pages/NewPlaces/NewPlaces.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './NewPlaces.css'
 import Input from '../../../shared/components/FormElements/Input/Input'
+import { VALIDATOR_REQUIRE } from '../../../shared/utils/validators'
 const NewPlaces = () => {
   return (
     <form className='place-form'>
-      <Input element="input" type="text" label="Title" validators={[]} errorText='Please Enter A Valid Title.' />
+      <Input element="input" type="text" label="Title" validators={[VALIDATOR_REQUIRE()]} errorText='Please Enter A Valid Title.' />
     </form>
   )
 }
