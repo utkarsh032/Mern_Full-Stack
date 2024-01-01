3ec668c259a1e358d28a7acb086a9d2fe459f819
utkarsh032 <utkarshraj525@gmail.com>
2024-01-01 21:04:34 +0530

    State in Input

--- frontend/src/places/pages/NewPlaces/NewPlaces.js
+++ frontend/src/places/pages/NewPlaces/NewPlaces.js
@@ -4,9 +4,8 @@ import Input from '../../../shared/components/FormElements/Input/Input'
 const NewPlaces = () => {
   return (
     <form className='place-form'>
-      <Input element="input" type="text" label="Title" />
+      <Input element="input" type="text" label="Title" validators={[]} errorText='Please Enter A Valid Title.' />
     </form>
   )
 }
-
-export default NewPlaces
+export default NewPlaces;
