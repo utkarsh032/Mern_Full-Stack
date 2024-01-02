ef01ac9d0cf2d21532dc1730e4309f85a8b14028
utkarsh032 <utkarshraj525@gmail.com>
2024-01-02 10:25:25 +0530

    updatePlace

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Users from './user/pages/Users'
 import NewPlaces from './places/pages/NewPlaces/NewPlaces'
 import MainNavigation from './shared/components/Navigation/MainNavigation/MainNavigation';
 import UserPlaces from './places/pages/UserPlaces';
+import UpdatePlace from '../src/places/pages/UpdatePlace'
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path='/' exact element={<Users />} />
           <Route path='/:userId/places' exact element={<UserPlaces />} />
           <Route path='/places/new' exact element={<NewPlaces />} />
+          <Route path='/places/:placeId' exact element={<UpdatePlace />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </main>
---
+++ frontend/src/places/pages/UpdatePlace.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import Input from '../../shared/components/FormElements/Input/Input';
+import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/utils/validators';
+import Button from '../../shared/components/FormElements/Button/Button';
+
+
+
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl: "https://media.timeout.com/images/101705309/image.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584
+    },
+    creator: 'u1'
+  },
+  {
+    id: 'p2',
+    title: "Empire State Building 2",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl: "https://media.timeout.com/images/101705309/image.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584
+    },
+    creator: 'u2'
+  }
+]
+
+
+const UpdatePlace = () => {
+  const placeId = useParams().placeId;
+  const identifiedPlace = DUMMY_PLACES.find(p => p.id === placeId);
+
+  if (!identifiedPlace) {
+    return <div className='center'><h2>Could NOt Find Place!</h2></div>
+  }
+  return (
+    <form>
+      <Input id="title" element="input" type="text" label="Title" validators={[VALIDATOR_REQUIRE()]} errorText="Please Enter a Valid Title" onInput={() => { }} value={identifiedPlace.title} valid={true} />
+
+      <Input id="description" element="textarea" type="description" label="Description" validators={[VALIDATOR_MINLENGTH(5)]} errorText="Please Enter a Valid Description" onInput={() => { }} value={identifiedPlace.description} valid={true} />
+
+      <Button>Update Place</Button>
+    </form>
+  )
+}
+
+export default UpdatePlace
