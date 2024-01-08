b5293252f794494e0e608c1dfb5bf2e984c94aae
utkarsh032 <utkarshraj525@gmail.com>
2024-01-08 19:33:03 +0530

    advantage of Express

--- NodeExpress_overview/app.js
+++ NodeExpress_overview/app.js
@@ -38,29 +38,19 @@ server.listen(3001)*/}
 
 //////////////// express  
 
+const bodyParser = require('body-parser')
+
 const express = require('express')
 const app = express()
 
-app.use((req, res, next) => {
+app.use(bodyParser.urlencoded({ extended: false }))
 
-  let body = '';
-  req.on('end', () => {
-    const userName = body.split('=')[1]
-    if (userName) {
-      req.body = { name: userName }
-    }
-    next()
-  })
-  req.on('data', chunk => {
-    body += chunk;
-  })
+app.post('/user', (req, res, next) => {
+  res.send('<h1>User:' + req.body.userName + '</h1>')
 })
 
-app.use((req, res, next) => {
-  if (req.body) {
-    return res.send('<h1>' + req.body.name + '</h1>')
-  }
-  res.send('<form method="post"><input type="text" name="userName"/><button>Create User</button></form>')
+app.get('/', (req, res, next) => {
+  res.send('<form action= "/user" method="post"><input type="text" name="userName"/><button>Create User</button></form>')
 })
 
 app.listen(5001)
