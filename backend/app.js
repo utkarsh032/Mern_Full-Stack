c4b1225c2358b8bf96e67ebed628c15312f90b3b
utkarsh032 <utkarshraj525@gmail.com>
2024-01-10 15:00:31 +0530

    handle_unSupported Routes

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const placesRoutes = require('./routes/places-routes');
 const bodyParser = require('body-parser');
-
+const HttpError = require('./models/http-error')
 const app = express();
+
 app.use(bodyParser.json())
 app.use('/api/places', placesRoutes)
+
+app.use((req, res, next) => {
+  const error = new HttpError('Couldn\'t find this routes.', 404)
+  throw error
+})
 
 app.use((error, req, res, next) => {
   if (res.headerSent) {
